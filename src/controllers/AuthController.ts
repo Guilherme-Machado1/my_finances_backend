065c9d60c48686ff761ecbe9af6f6536b588cc13
Guilherme Santos <guilhermemachado1900@gmail.com>
2023-02-11 15:57:23 -0300

    build: add type to the functions

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,21 @@
+import { HttpResponse } from "../interfaces/interfaces";
 import { HttpRequest } from "../interfaces/interfaces"
-import AuthService from "../services/AuthService"
+import { AuthServiceProtocol } from "../protocols/AuthServiceProtocol";
 class AuthController {
-  public authService = new AuthService()
-  public signUp(): any{
-    const body: HttpRequest = {name: 'any_name', email: 'any_email', password: 'any_password'}
+  private authService: AuthServiceProtocol
+  constructor(authService: AuthServiceProtocol){
+    this.authService = authService;
+  }
+  public signUp(): Promise<HttpResponse>{
+    const body: HttpRequest = {body: {name: 'any_name', email: 'any_email', password: 'any_password'}}
     const userSignUp = this.authService.signUp(body)
+    return userSignUp;
+
+  }
+
+  public signIn(): Promise<HttpResponse>{
+    const body: HttpRequest = {body: {email: 'any_email', password: 'any_password'}}
+    const userSignUp = this.authService.signIn(body)
     return userSignUp;
 
   }
