c602aa23613bb55e59100a4787f4606e116be6dd
Guilherme Santos <guilhermemachado1900@gmail.com>
2023-02-11 16:00:37 -0300

    build: generates the JWT token

---
+++ src/helper/JWTGenerator.ts
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken'
+import * as dotenv from 'dotenv'
+import path from 'path'
+dotenv.config({path: path.resolve(__dirname, '..', '..', '.env')})
+
+const secret: string = process.env.secret!
+
+interface JWTPayload {
+  email: string;
+  name: string;
+  exp: number;
+  iat: number;
+}
+
+interface UserInterface {
+  email: string;
+  name: string;
+}
+class JWTGenerator {
+  public generateJWT(user: UserInterface){
+    const token = jwt.sign({
+      name: user.name,
+      email: user.email
+    }, secret, {
+      expiresIn: '1d'
+    }) as string;
+
+    return token;
+  }
+
+  public verify(token: string){
+    const decode = jwt.verify(token, secret);
+    return decode;
+  }
+}
+
+export default JWTGenerator;
