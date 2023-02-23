008d221733bdb999f86c4803084d05b9c2c14a26
Guilherme Santos <guilhermemachado1900@gmail.com>
2023-02-23 20:12:09 -0300

    test: add validation tests

--- src/services/AuthService.spec.ts
+++ src/services/AuthService.spec.ts
@@ -1,7 +1,6 @@
 import EmailValidator from "../helper/EmailValidator";
 import EncryptPassword from "../helper/EncryptPassword";
 import AuthService from "./AuthService"
-
 const createSut = () => {
   const encrypter = new EncryptPassword(10)
   const emailValidator = new EmailValidator()
@@ -62,6 +61,53 @@ describe('Auth tests', () => {
     expect(signup.body).toEqual('E-mail is not valid')
   })
 
+  it('should return return a hashed Password', async() => {
+    const { sut } = createSut()
+    const dummyUser = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    };
+
+    const expectedResponse = {
+      statusCode: 200,
+      body: {
+        id: 'd6ac25cf-3014-452e-ae42-137874bae951',
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: '$2b$10$6S4D0Rmvmke0GXMipXee1O6SpECJnsoQEUiuxWhLlJiC1HANR5WCa'
+      }
+    }
+
+  jest.spyOn(sut, 'signUp').mockResolvedValue(expectedResponse);
+    const response = await sut.signUp(dummyUser)
+    const pass = response.body.password
+    const id = response.body.id
+    expect(pass).toBeDefined()
+    expect(id).toBeDefined()
+  })
+
+  it('should return status 400 if E-mail is already in use', async() => {
+    const { sut } = createSut()
+    const dummyUser = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    }
+    const expectedResponse = {
+      statusCode: 400,
+      body: 'The email is already registered'
+    }
+    jest.spyOn(sut, 'signUp').mockResolvedValue(expectedResponse);
+    const signup = await sut.signUp(dummyUser);
+    expect(signup.statusCode).toBe(400)
+    expect(signup.body).toEqual('The email is already registered')
+  })
+
   it('should return status 200 if it is all good', async() => {
     const { sut } = createSut()
     const dummyUser = {
@@ -71,9 +117,16 @@ describe('Auth tests', () => {
         password: 'any_password'
       }
     }
-    const signup = await sut.signUp(dummyUser);
-    const hashPassword = await signup.body.body.password
-    expect(signup.statusCode).toBe(200)
-    expect(signup.body.body).toEqual({name: 'any_name', email: 'any_email@gmail.com', password: hashPassword})
+
+    const expectedResponse = {
+      statusCode: 200,
+      body: {id: 'any_id',name: 'any_name', email: 'any_email@gmail.com', password: 'any_password_hashed'}
+    };
+
+    const spySignUp = jest.spyOn(sut, 'signUp').mockResolvedValue(expectedResponse);
+    const response = await sut.signUp(dummyUser);
+    expect(spySignUp).toHaveBeenCalledWith(dummyUser);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({id: 'any_id',name: 'any_name', email: 'any_email@gmail.com', password: 'any_password_hashed'});
   })
 })
