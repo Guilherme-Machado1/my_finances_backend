db87fe6ea95ad2a6f36fe3befe3d32df82cdbb5d
Guilherme Santos <guilhermemachado1900@gmail.com>
2023-02-23 20:08:08 -0300

    test: test if the decrypt function is decrypting

--- src/helper/EncrypterPassword.spec.ts
+++ src/helper/EncrypterPassword.spec.ts
@@ -1,9 +1,17 @@
 import EncryptPassword from "./EncryptPassword";
 describe('Encrypter tests', () => {
-  it('should not return the password without the encryptation ', async() => {
+  it('should not return the password without the encryption ', async() => {
     const encrypter = new EncryptPassword(10);
     const password = '123456'
     const passwordHashed = encrypter.encrypt(password);
     expect(passwordHashed).not.toBe(password)
+  })
+
+  it('should return true if the password is equal to the hasshed Password', async() => {
+    const encrypter = new EncryptPassword(10);
+    const password = '123456'
+    const passwordHashed = '$2b$10$G35jPHtcPmHpeL2eUOPQNegaTrnMMNxfihOdOanugwjHoTdGxNyYm'
+    const decryptPassword = await encrypter.decrypt(password, passwordHashed);
+    expect(decryptPassword).toBe(true)
   })
 })
