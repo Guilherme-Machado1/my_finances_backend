8fe00ddd4a6303a6a07f707b90b9a66e6b0a2a5d
Guilherme Santos <guilhermemachado1900@gmail.com>
2023-03-14 16:41:02 -0300

    build: transform in class

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,34 @@
-import express, { Application, Request, Response } from 'express';
+import 'express-async-errors'
+import express, { Application} from 'express';
+import { errorMiddleware } from './middleware/error';
 import authRoutes from './routes/auth.routes'
 
-const app: Application = express();
+class App {
+  private app: Application;
 
-// Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/', authRoutes)
+  constructor() {
+    this.app = express();
+    this.configureMiddlewares();
+    this.configureRoutes();
+    this.startServer();
+  }
 
+  private configureMiddlewares(): void {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(errorMiddleware);
+  }
 
-// Listen to port
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  private configureRoutes(): void {
+    this.app.use('/', authRoutes);
+  }
+
+  private startServer(): void {
+    const port = 3001;
+    this.app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  }
+}
+
+new App();
