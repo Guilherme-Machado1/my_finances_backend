6d5eb10337eb985e688dd57ed0a93842bfcde049
Guilherme Santos <guilhermemachado1900@gmail.com>
2023-03-14 16:42:20 -0300

    build: add SignIn function

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,17 +1,24 @@
 import { HttpResponse, HttpRequest } from "../interfaces/interfaces"
-import { badRequest, success } from "../protocols/returnFunctions"
+import { badRequest, success, unauthorized } from "../protocols/returnFunctions"
 import { EncrypterProtocol } from "../protocols/EncrypterProtocol"
 import { EmailValidatorProtocol } from "../protocols/EmailValidatorProtocol"
 import { v4 as uuidv4 } from 'uuid';
 import user from "../models/User";
 import { AuthServiceProtocol } from "../protocols/AuthServiceProtocol";
+import { JwtProtocol } from "../protocols/JwtProtocol";
 
 class AuthService implements AuthServiceProtocol {
   private encryptPassword: EncrypterProtocol
   private emailValidator: EmailValidatorProtocol
-  constructor(encryptPassword: EncrypterProtocol, emailValidator: EmailValidatorProtocol){
+  private jwt: JwtProtocol
+  constructor(
+    encryptPassword: EncrypterProtocol,
+    emailValidator: EmailValidatorProtocol,
+    jwt: JwtProtocol
+    ){
     this.encryptPassword = encryptPassword;
-    this.emailValidator = emailValidator
+    this.emailValidator = emailValidator;
+    this.jwt = jwt
   }
   public async signUp(userData: HttpRequest): Promise<HttpResponse>{
     const requiredFields = ['name', 'email', 'password']
@@ -44,8 +51,29 @@ class AuthService implements AuthServiceProtocol {
   }
 
   public async signIn(userData: HttpRequest): Promise<HttpResponse>{
+    const requiredFields = ['email', 'password']
+    for(let field of requiredFields){
+      if(!userData.body[field]){
+        return badRequest(`The propertie ${field} is missing`)
+      }
+    }
 
-    return success(userData)
+    const userExists = await user.findUserByEmail(userData.body.email)
+
+    if(userExists?.length! === 0){
+      return badRequest(`The email does not exists`)
+    }
+
+    const decryptPassword = await this.encryptPassword.decrypt(userData.body.password, userExists![0].password)
+
+    if(!decryptPassword){
+      return unauthorized('The password is incorrect')
+    }
+
+
+    const jwt = this.jwt.generateJWT(userExists![0].email, userExists![0].name)
+
+    return success({...userData, jwt})
   }
 }
 
